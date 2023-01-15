5f01b104f67f1eb84ca1d8e3b9fe65369597a0c3
dichenk <dchenk@DESKTOP-R5OCG73.localdomain>
2023-01-15 15:51:21 +0300

    отработал парсинг и сохранение данных. осталась распаковка таблиц по запросу пользователя и сортировка-фильтрация данных

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import os
 import pandas as pd
 from abc import abstractmethod
@@ -16,25 +15,33 @@ class Engine():
 class HeadHunter(Engine):
     __link = 'https://api.hh.ru/vacancies'
 
-    def __init__(self):
-        self.__vacancy = self.__get_request()
-        print(len(self.__vacancy))
-        
+    def __init__(self, nm):
+        self.__vacancy = self.__get_request(nm)
+        print(self.__vacancy.loc[1]['employer']['name'])
+
+    
+    def __len__(self):
+        return len(self.__vacancy)
+
 #    @property
 #    def vacancy(self):
 #        return self.__vacancy
 
-    @classmethod
-    def __get_request(cls):
+    def __get_request(self, nm):
+        if os.path.isfile('hh_vac.txt'):
+            os.remove('hh_vac.txt')
         params = {
-                'text': 'Python разработчик',
+                'text': nm,
                 'page': 1,
                 'per_page': 100
                 }
         response = pd.DataFrame()
-        for i in range(6):
+        for i in range(1):
             params['page'] = i
-            response_big = requests.get(cls.__link, params).json()
+            try:
+                response_big = requests.get(self.__link, params).json()
+            except:
+                continue
             response_big = pd.DataFrame.from_dict(response_big)
             response_items = response_big.loc[:]['items']
             response_small = pd.DataFrame(list(response_items))
@@ -45,22 +52,26 @@ class HeadHunter(Engine):
 class SuperJob(Engine):
     __link = 'https://api.superjob.ru/2.0/vacancies/'
 
-    def __init__(self):
-        self.__vacancy = self.__get_request()
-        print(self.__vacancy)
-        print(len(self.__vacancy))
+    def __init__(self, nm):
+        self.__vacancy = self.__get_request(nm)
+        print(self.__vacancy.loc[1]['firm_name'])
+
+
+    
+    def __len__(self):
+        return len(self.__vacancy)
     
 #    @property
 #    def vacancy(self):
 #        return self.__vacancy
     
-    @classmethod
-    def __get_request(cls):
+    def __get_request(self, nm):
+        if os.path.isfile('sj_vac.txt'):
+            os.remove('sj_vac.txt')
         params = {
-                'keyword': 'python',
+                'keyword': nm,
                 'page': 1,
                 'count': 100
-                #        'page': 2
                 }
         headers = {
                 'Host': 'api.superjob.ru',
@@ -69,42 +80,57 @@ class SuperJob(Engine):
                 'Content-Type': 'application/x-www-form-urlencoded'
                 }
         response = pd.DataFrame()
-        for i in range(0, 6):
+        for i in range(1):
             params['page'] = i
-            response_big = requests.get(cls.__link, headers=headers, params=params).json()
+            try:
+                response_big = requests.get(self.__link, headers=headers, params=params).json()
+            except:
+                continue
             response_big = pd.DataFrame.from_dict(response_big)
             response_items = response_big.loc[:, 'objects']
             response_small = pd.DataFrame(list(response_items))
             response = pd.concat([response, response_small], axis=0)
         response.to_csv('sj_vac.txt', sep=';' )
-
         return response
         
 class Vacancy:
-    def __init__(self, nm = None, lnk = None, abt = None, slr = None):
-        self.__name = nm
-        self.__link = lnk
-        self.__about = abt
-        self.__salary = slr
-        print('before')
-        self.__whatever = self.__take_from_sj()
-        print('first complited')
-#        self.__whatever2 = self.__take_from_hh()
-#        print('second complited')
-
+    def __init__(self, vac_name = 'python'):
+        self.__vac_key = vac_name
+        self.__name = []
+        self.__salary = []
+        self.__place_of_work = []
+        self.__experience = []
+        self.__vac_hh = self.__take_from_hh(vac_name)
+        self.printing()
+        self.__vac_sj = self.__take_from_sj(vac_name)
+        self.amount_of_vacancies = len(self.__vac_hh) + len(self.__vac_sj)
+    
+    def printing(self):
+        for i in range(10):
+            print('.', end='')
+            time.sleep(1)
+            if i == 5:
+                print('да-да, я все еще ищу', end=' ')
+        print()
+    
+    def find_me_offer(self, a, b, c):
+        self.__vac_hh
+        print(a, b, c)
+        
     @staticmethod
-    def __take_from_hh(what = None):
-        hh = HeadHunter()
+    def __take_from_hh(nm):
+        hh = HeadHunter(nm)
         return hh
 
     @staticmethod
-    def __take_from_sj(what = None):
-        sj = SuperJob()
+    def __take_from_sj(nm):
+        sj = SuperJob(nm)
         return sj
     
     def __repr__(self):
-        return f'name: {self.__name}\nlnk: {self.__link},\nabout: {self.__about},\nsalary: {self.__salary}'
+        #return f'name: {self.__name}\nlnk: {self.__link},\nabout: {self.__about},\nsalary: {self.__salary}'
+        return str(len(self.__whatever) + len(self.__whatever2))
 
 #    @property
 #    def name(self, nm):
-        self.__name = nm
+#        self.__name = nm
--- src/request.py
+++ src/request.py
@@ -1,3 +1,36 @@
 import classes
 
-new_elem = classes.Vacancy()
+new_elem = vacancy_text = vacancy_salary = vacancy_place_of_work = vacancy_experience = None
+while 1:
+    vacancy_text = input('Что ищем?\nВведите ключевую фразу:')
+    while 1:
+        try:
+            vacancy_salary = input('Сколько денег?\nВведите целое число от 0 до 1000000: ')
+            vacancy_salary = int(vacancy_salary)
+            if 0 < vacancy_salary < 1000000:
+                break
+        except:
+            pass
+    while 1:
+        vacancy_place_of_work = input('удаленка?\nВведите да или нет: ')
+        if vacancy_place_of_work in ['да', 'нет']:
+            break
+    while 1:
+        vacancy_experience = input('опыт?\nВведите число от 0 до 99: ')
+        try:
+            vacancy_experience = int(vacancy_experience)
+            if 0 < vacancy_experience < 100:
+                break
+        except:
+            pass
+
+    new_elem = classes.Vacancy(vacancy_text)
+    if new_elem.amount_of_vacancies == 0:
+        print('\t\t\tНичего не найдено\nПереформулируйте ваш запрос!\n')
+        continue
+    break
+
+new_elem.find_me_offer(vacancy_salary, vacancy_place_of_work, vacancy_experience)
+
+
+
